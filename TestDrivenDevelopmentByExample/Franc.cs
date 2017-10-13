e22b0e91ce8832fb2a1472232467cdf6fad44767
puye.xiongtai <puye.xiongtai@gmail.com>
2017-10-14 00:17:11 +0900

    第５章　原則をあえて破るとき

---
+++ TestDrivenDevelopmentByExample/Franc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopmentByExample
+{
+    public class Franc
+    {
+        private int amount;
+        public Franc(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Franc()
+        {
+        }
+
+        public Franc times(int multiplier)
+        {
+            return new Franc(amount * multiplier);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Franc franc = (Franc)obj;
+            return amount == franc.amount;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.amount;
+        }
+    }
+}
