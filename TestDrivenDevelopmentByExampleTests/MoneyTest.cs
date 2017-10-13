0e9b2a0a9a93f1cc0c9437b1e6dd33ef23f39765
puye.xiongtai <puye.xiongtai@gmail.com>
2017-10-14 00:10:51 +0900

    第４章　意図を語るテスト

--- TestDrivenDevelopmentByExampleTests/MoneyTest.cs
+++ TestDrivenDevelopmentByExampleTests/MoneyTest.cs
@@ -15,10 +15,8 @@ namespace TestDrivenDevelopmentByExample.Tests
         public void testMultiplication()
         {
             Dollar five = new Dollar(5);
-            Dollar product = five.times(2);
-            Assert.AreEqual(new Dollar(10), product);
-            product = five.times(3);
-            Assert.AreEqual(15, product.amount);
+            Assert.AreEqual(new Dollar(10), five.times(2));
+            Assert.AreEqual(new Dollar(15), five.times(3));
         }
 
         [TestMethod()]
