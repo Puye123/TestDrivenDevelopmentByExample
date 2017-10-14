99a20cd251360253b64be14cdcb1b3f6aea3ea85
puye.xiongtai <puye.xiongtai@gmail.com>
2017-10-14 11:08:55 +0900

    第１１章　不要になったら消す スッキリ！

--- TestDrivenDevelopmentByExampleTests/MoneyTest.cs
+++ TestDrivenDevelopmentByExampleTests/MoneyTest.cs
@@ -24,17 +24,7 @@ namespace TestDrivenDevelopmentByExample.Tests
         {
             Assert.IsTrue(Money.Dollar(5).Equals(Money.Dollar(5)));
             Assert.IsFalse(Money.Dollar(5).Equals(Money.Dollar(6)));
-            Assert.IsTrue(Money.Franc(5).Equals(Money.Franc(5)));
-            Assert.IsFalse(Money.Franc(5).Equals(Money.Franc(6)));
             Assert.IsFalse(Money.Franc(5).Equals(Money.Dollar(5)));
-        }
-
-        [TestMethod()]
-        public void testFrancMultiplication()
-        {
-            Money five = Money.Franc(5);
-            Assert.AreEqual(Money.Franc(10), five.Times(2));
-            Assert.AreEqual(Money.Franc(15), five.Times(3));
         }
 
         [TestMethod()]
@@ -42,13 +32,6 @@ namespace TestDrivenDevelopmentByExample.Tests
         {
             Assert.AreEqual("USD", Money.Dollar(1).Currency());
             Assert.AreEqual("CHF", Money.Franc(1).Currency());
-        }
-
-        [TestMethod()]
-        public void testDifferentClassEquality()
-        {
-            Assert.IsTrue(new Money(10, "CHF").Equals(new Franc(10, "CHF")));
-            Assert.IsTrue(new Money(10, "USD").Equals(new Dollar(10, "USD")));
         }
     }
 }
